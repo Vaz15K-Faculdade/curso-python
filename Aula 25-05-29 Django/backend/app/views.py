29dc58fe9f3e476f5054064089e1e9dfb1fa0ced
Keven Eduardo <79882241+Vaz15k@users.noreply.github.com>
2025-05-29 23:35:52 +0000

    Importação do projeto em django
    apenas por que o professor pediu para usar...

---
+++ Aula 25-05-29 Django/backend/app/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render
+
+from app.models import Produto, Venda
+
+def home_view(request):
+    return render(
+        request, 
+        'home.html', 
+        {
+            'paginas': [
+                {'nome': 'Vendas', 'url': '/vendas'},
+                {'nome': 'Produtos', 'url': '/produtos'},
+                # {'nome': 'Formas de Pagamento', 'url': '/formasPagamento'},
+                # {'nome': 'Categorias', 'url': '/categorias'},
+            ]
+        }
+    )
+
+# Create your views here.
+def produtos_view(request):
+    # equivalente ao "SELECT * FROM Produto"
+    produtos = Produto.objects.all()
+    
+    return render(request, 'produtos.html', {'produtos': produtos})
+
+def vendas_view(request):
+    vendas = Venda.objects.prefetch_related('produtos_vendidos__produto')
+    # vendas = Venda.objects.all()
+
+    return render(request, 'vendas.html', {'vendas': vendas})
